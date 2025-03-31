d4d3f8724205707859f0fe66723f0e85867e4ed7
Elvisqk <15713299982@163.com>
2025-03-31 18:24:30 +0800

    更新

--- Final/OS/nachos-3.4/code/lab7-8-old/arch/unknown-i386-linux/depends/thread.d
+++
@@ -1,8 +0,0 @@
-arch/unknown-i386-linux/depends/thread.d arch/unknown-i386-linux/objects/thread.o: thread.cc ../threads/copyright.h thread.h ../threads/utility.h \
- ../threads/copyright.h ../threads/bool.h ../machine/sysdep.h \
- ../machine/machine.h ../machine/translate.h ../machine/disk.h \
- ../userprog/addrspace.h ../filesys/filesys.h ../filesys/openfile.h \
- ../threads/switch.h ../threads/synch.h ../threads/thread.h \
- ../threads/list.h ../threads/utility.h ../threads/system.h \
- ../threads/scheduler.h ../machine/interrupt.h ../threads/list.h \
- ../machine/stats.h ../machine/timer.h
